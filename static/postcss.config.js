5cc6f69f5c88bda8d6d95cfc6e26497ee7e6b7d2
wanjirumurira <wanjirumurira@gmail.com>
2024-06-14 15:10:18 +0300

    readme update

--- static/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
-module.exports = {
-  plugins: [
-    purgecss({
-      content: ['./*.html'] // Adjusted the path to match the correct location of HTML files
-    })
-  ]
-}
